5232297afd10bda1789254f4ca175c8b11ff4162
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-26 21:31:17 -0800

    add finishing touches to crew page

--- tabs.js
+++ tabs.js
@@ -15,24 +15,22 @@ function changeTabFocus(e) {
   // Change tabindex of current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
-  }
 
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
   }
-
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
@@ -49,15 +47,19 @@ function changeTabPanel(e) {
 
   targetTab.setAttribute("aria-selected", true);
 
-  mainContainer
-    .querySelectorAll("[role='tabpanel']")
-    .forEach((panel) => panel.setAttribute("hidden", true));
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, `#${targetPanel}`);
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, `#${targetImage}`);
+}
 
-  mainContainer
-    .querySelectorAll("picture")
-    .forEach((img) => img.setAttribute("hidden", true));
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
 
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
 }
